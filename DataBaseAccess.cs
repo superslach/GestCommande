84d613df24eab40975cf3e97068259f7e0d0a2a4
superslach <32452000+superslach@users.noreply.github.com>
2017-10-12 15:23:11 +0200

    Add files via upload

---
+++ DataBaseAccess.cs
@@ -0,0 +1,45 @@
+﻿
+using MySql.Data.MySqlClient;
+
+namespace EasyActiveRecord
+{
+    static class DataBaseAccess
+    {
+        private static MySqlConnection _connexion = new MySqlConnection("Database=Contacts;Data Source=localhost;User Id=root;Password=root");
+
+        public static MySqlConnection Connexion
+        {
+            get
+            {
+                return _connexion;
+            }
+
+            set
+            {
+                _connexion = value;
+            }
+        }
+
+        public static MySqlParameter CodeParam(string paramName, object value)
+        {
+            MySqlCommand commandSql = Connexion.CreateCommand();
+            MySqlParameter parametre = commandSql.CreateParameter();
+            parametre.ParameterName = paramName;
+            parametre.Value = value;
+            return parametre;
+        }
+    }
+}
+
+//Avec Abstraction -> Diminue le COUPLAGE 
+
+//public static IDbConnection Connexion { get; set; }
+
+//public static IDbDataParameter CodeParam(string paramName, object value)
+//{
+//    IDbCommand commandSql = Connexion.CreateCommand();
+//    IDbDataParameter parametre = commandSql.CreateParameter();
+//    parametre.ParameterName = paramName;
+//    parametre.Value = value;
+//    return parametre;
+//}
